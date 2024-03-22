13d09b93ed8fd1dd576db91a71c6800d81132a10
TudoHuang <88125758+tudohuang@users.noreply.github.com>
2024-03-22 15:52:53 +0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,10 +1,6 @@
 from flask import Flask, request, render_template, jsonify
 from markupsafe import Markup
 import sqlite3
-import torch
-import torch.nn as nn
-from tensorflow.keras.preprocessing.text import tokenizer_from_json
-from tensorflow.keras.preprocessing.sequence import pad_sequences
 import json
 from markdown.extensions.codehilite import CodeHiliteExtension
 from markdown.extensions.tables import TableExtension
@@ -16,46 +12,7 @@ UPLOAD_FOLDER = r'C:\Gdrive_tudo\spell_training\flaskwebb\mdp'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-# 模型定義
-class TextLSTM(nn.Module):
-    def __init__(self, vocab_size, embed_dim, hidden_dim, num_layers, num_classes):
-        super(TextLSTM, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, embed_dim)
-        self.lstm = nn.LSTM(embed_dim, hidden_dim, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_dim, num_classes)
 
-    def forward(self, x):
-        x = self.embedding(x)
-        x, _ = self.lstm(x)
-        x = x[:, -1, :]
-        x = self.fc(x)
-        return x
-
-# 加載模型和 tokenizer
-with open("tokenizer_config.json") as f:
-    tokenizer_data = f.read()
-tokenizer = tokenizer_from_json(tokenizer_data)
-
-model = TextLSTM(vocab_size=10000, embed_dim=100, hidden_dim=128, num_layers=2, num_classes=6)
-model.load_state_dict(torch.load("lstm.pt", map_location=torch.device('cpu')))
-model.eval()
-
-# 預處理函數
-def preprocess_data(sentence):
-    test_tokenized = tokenizer.texts_to_sequences([sentence])
-    test_padded = pad_sequences(test_tokenized, maxlen=171)
-    test_padded_tensor = torch.tensor(test_padded, dtype=torch.long)
-    return test_padded_tensor
-
-# 預測情緒函數
-def predict_emotion(text):
-    preprocessed_text = preprocess_data(text)
-    with torch.no_grad():
-        prediction = model(preprocessed_text)
-        predicted_class = torch.argmax(prediction, dim=1)
-        result = predicted_class.item()
-        emotions = ["Sadness", "Joy", "Love", "Anger", "Fear", "Surprise"]
-        return emotions[result]
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -81,12 +38,7 @@ def login():
     
     return render_template("login.html", error=error)
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    data = request.json
-    text = data.get("text", "")
-    emotion = predict_emotion(text)
-    return jsonify({"emotion": emotion})
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
