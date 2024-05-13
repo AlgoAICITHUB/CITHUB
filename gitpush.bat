4bb99d979556dca3f6ae93bb42aca49343836973
tudo1211tw@gmail.com <tudohuang>
2024-05-13 14:52:01 +0800

    redteaming_class

--- gitpush.bat
+++ gitpush.bat
@@ -1,12 +1,16 @@
 @echo off
+::檢查是否有commit訊息
 if "%1" == "" (
     echo message!!
     echo method: gitpush "msseage"
     exit /b 1
 )
 echo Adding...
+::設定名字
 git config --global user.email %2
+
 git config --global user.name %3
+::提交
 git add .
 echo Message: %1
 git commit -m %1
